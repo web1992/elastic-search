177ac56dd52e654b0523242464ac9222566de4d7
web1992 <554933654@qq.com>
2022-10-24 21:51:23 +0800

    fix config

--- src/main/java/cn/web1992/es/config/EsConfiguration.java
+++ src/main/java/cn/web1992/es/config/EsConfiguration.java
@@ -2,6 +2,10 @@ package cn.web1992.es.config;
 
 
 import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
@@ -53,6 +57,10 @@ public class EsConfiguration {
     @Bean
     @PostConstruct
     public RestHighLevelClient client() {
+
+        final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials("elastic", "dSdnCw_dkZoRxWXGR6Sw"));  //es账号密码（默认用户名为elastic）
+
         List<HttpHost> hostList = this.buildHosts(address);
         RestClientBuilder builder = RestClient.builder(hostList.toArray(new HttpHost[0]));
 
@@ -67,6 +75,9 @@ public class EsConfiguration {
         builder.setHttpClientConfigCallback(httpClientBuilder -> {
             httpClientBuilder.setMaxConnTotal(maxConnectNum);
             httpClientBuilder.setMaxConnPerRoute(maxConnectPerRoute);
+
+            httpClientBuilder.disableAuthCaching();
+            httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
             return httpClientBuilder;
         });
         RestHighLevelClient client = new RestHighLevelClient(builder);
